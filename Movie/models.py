eeac184f83be9ac9f13041adf88fb918e1dc4f19
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-18 09:09:04 +0800

    migrated models

--- Movie/models.py
+++ Movie/models.py
@@ -6,17 +6,24 @@ class Film(models.Model):
     Author = models.ForeignKey('auth.User')
     Title = models.CharField(max_length=200)
     Description = models.TextField()
-    Created_date = models.DateTimeField(
-            default=timezone.now)
-    Published_date = models.DateTimeField(
-            blank=True, null=True)
+    Created_date = models.DateTimeField(default=timezone.now)
+    Published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    actor = models.ManyToManyField ("Actor", related_name="Film")
+    genre = models.ManyToManyField ("Genre", related_name="Film")
 
     def __str__(self):
-        return self.title
+    	return "{} by {} - {}".format(self.Title, self.list_actor(), self.list_genre())
+
+    def list_actor(self):
+    	return ",".join([actor.Actor_name for actor in self.actor.all()])
+
+    def list_genre(self):
+    	return ",".join([genre.Type for genre in self.genre.all()])
+
+    def save(self, *args, **kwargs):
+    	self.published_date = timezone.now()
+    	super(Film, self).save(*args, **kwargs)
 
 class Actor(models.Model):
 	Actor_name = models.CharField(max_length=200)

d8a1a9cfc616ace63b474af3c544b57e638089c4
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-18 11:57:10 +0800

    modified views.py and film.html

--- Movie/views.py
+++ Movie/views.py
@@ -7,15 +7,16 @@ from django.views.generic import View
 class film_list(View):
 	def get(self, request):
 		film = Film.objects.all()
+		#films = Film.objects.filter(Published_date__lte=timezone.now()).order_by('published_date')
 		actor = Actor.objects.all()
 		genre = Genre.objects.all()
 		context = {
 		'film': film,
+		#'films': films,
 		'actor': actor,
 		'genre': genre,
 		}
-		return render(request, "film_list.html", context)
-
+		return render(request, "film_list.html", {'film': film})
 
 # for generic view
 #class film_list(View):

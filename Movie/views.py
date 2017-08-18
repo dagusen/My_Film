5485ede4ce18affd6d38d5846138b14d20f555d9
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-18 09:41:28 +0800

    added urls to app Movie and createdd a view for film_list

--- Movie/views.py
+++ Movie/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Film, Actor, Genre
+from django.views.generic import View
 # Create your views here.
+
+def film_list(request):
+    return render(request, "film_list.html", {})

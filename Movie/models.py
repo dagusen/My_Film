2a1960effe188d82f1bf2737c8e8b0594f8adc1d
Louis <louisaleksieb.dagusen@gmail.com>
2017-08-18 09:02:07 +0800

    added tables to models

--- Movie/models.py
+++ Movie/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Film(models.Model):
+    Author = models.ForeignKey('auth.User')
+    Title = models.CharField(max_length=200)
+    Description = models.TextField()
+    Created_date = models.DateTimeField(
+            default=timezone.now)
+    Published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Actor(models.Model):
+	Actor_name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.actor_name
+
+class Genre(models.Model):
+	Type = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.Type
